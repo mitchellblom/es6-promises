d90df9badef103ddae52f3a671d3097abddb9949
Mitchell Blom <mitchellblom@gmail.com>
2017-04-18 20:30:42 -0500

    humans and animals writing to dom - no es6

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,30 @@
 
 $(document).ready(function() {
 
+    var outputContainer = $("#output");
+
+    var writeToDOM = function(humanArray) {
+        var domString = "";
+        for (var i = 0; i < humanArray.length; i++) {
+            domString += `<div class="human row">`;
+            domString += `<div class="col-sm-4">`;
+            domString += `<img src="${humanArray[i].image}">`;
+            domString += `<p>${humanArray[i].name}</p>`;
+            domString += `</div>`;
+            domString += `<div class="col-sm-8 overflow-row">`;
+            for (var j = 0; j < humanArray[i].matches.length; j++) {
+                domString += `<div class="animal">`;
+                domString += `<img src="${humanArray[i].matches[j].image}">`;
+                domString += `<p>${humanArray[i].matches[j].name}</p>`;
+                domString += `<p>${humanArray[i].matches[j].description}</p>`;
+                domString += `</div>`;
+            }
+            domString += `</div>`;
+            domString += `</div>`;
+        }
+        outputContainer.append(domString);
+    };
+
     var loadHumans = function() {
         return new Promise(function(resolve, reject) {
             $.ajax("./database/humans.json")
@@ -58,17 +82,27 @@ $(document).ready(function() {
     var checkForTypeMatch = function(human, pet) {
         var interestedInArray = human["interested-in"];
         var isMatchNumber = interestedInArray.indexOf(pet.type);
-        if (isMatchNumber === -1){
-        	return false;
+        if (isMatchNumber === -1) {
+            return false;
         } else {
-        	return true; // will be either true or false
-        };
+            return true; // will be either true or false
+        }
+    };
+
+    var checkForKidFrendly = function(human, pet){
+    	var hasKids = human["has-kids"];
+    	var isKidFriendly = pet["kid-friendly"];
+    	var isMatched = true;
+    	if (hasKids && !isKidFriendly){
+    		isMatched = false;
+    	}
+    	return isMatched;
     };
 
     loadHumans().then(function(humans) {
         // myHumans.push(humans)
         humans.forEach(function(human) {
-            human.matches = [];		// adding a key on humans that is an empty array, to push matches that fit
+            human.matches = []; // adding a key on humans that is an empty array, to push matches that fit
             myHumans.push(human);
         });
 
@@ -80,26 +114,26 @@ $(document).ready(function() {
                     });
                 });
 
-                for (var i = 0; i < myHumans.length; i++){
-                	for (var j = 0; j <myAnimals.length; j++){
-                		if (checkForTypeMatch(myHumans[i], myAnimals[j])) {
-                			myHumans[i].matches.push(myAnimals[j]);
-                		}
-                	}
+                for (var i = 0; i < myHumans.length; i++) {
+                    for (var j = 0; j < myAnimals.length; j++) {
+                        if (checkForTypeMatch(myHumans[i], myAnimals[j]) && checkForKidFrendly(myHumans[i], myAnimals[j])) {
+                            myHumans[i].matches.push(myAnimals[j]);
+                        }
+                    }
                 }
-                console.log(myHumans);
-            });
+                writeToDOM(myHumans);
+            })
+            .catch(function(animalErrors){
+            	console.log(animalErrors);
+            })
+    })
+    .catch(function(humanError){
+    	console.log(humanError);
     });
 
 
 
     // hs -c-1 runs hs without cacheing issues
-
-
-
-
-
-
 
 
 
